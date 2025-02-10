7260dfa9c611d2bcc65f50ea954eb52ec0872e0a
kbodur <kbodur@pursuit.org>
2025-02-10 11:40:03 -0500

    Lesson02-strings  completed

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -6,21 +6,21 @@
  */
 function lengthOfLongestSubstring(s) {
 
-  // let maxLength = 0;
-  // let currentSubstring = '';
+  let maxLength = 0;
+  let currentSubstring = '';
 
-  // for (let i = 0; i < s.length; i++) {
-  //   const char = s[i];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
 
-  //   if (!currentSubstring.includes(char)) {
-  //     currentSubstring += char;
-  //     maxLength = Math.max(maxLength, currentSubstring.length);
-  //   } else {
-  //     currentSubstring = currentSubstring.slice(currentSubstring.indexOf(char) + 1) + char;
-  //   }
-  // }
+    if (!currentSubstring.includes(char)) {
+      currentSubstring += char;
+      maxLength = Math.max(maxLength, currentSubstring.length);
+    } else {
+      currentSubstring = currentSubstring.slice(currentSubstring.indexOf(char) + 1) + char;
+    }
+  }
 
-  // return maxLength;
+  return maxLength;
 }
 
 
